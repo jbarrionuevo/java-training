65d0bd65bcacf17912ce018085eaf3472ca3bd31
Luiggi Thomas Mendoza Jarahuanco <luiggi.mendoza@globant.com>
2016-06-14 19:21:10 -0500

    Updated examples in day 1 package. Added new examples in day 1 package.

--- java-training-core/src/main/java/edu/globant/day1/ShapeClient.java
+++ java-training-core/src/main/java/edu/globant/day1/ShapeClient.java
@@ -5,6 +5,7 @@ import static java.util.Arrays.asList;
 import java.util.List;
 
 import edu.globant.day1.subclasses.Circle;
+import edu.globant.day1.subclasses.Rectangle;
 import edu.globant.day1.subclasses.Square;
 import edu.globant.day1.subclasses.Triangle;
 import edu.globant.day1.superclasses.Shape;
@@ -13,8 +14,8 @@ public class ShapeClient {
 
 	public static void main(String[] args) {
 		//uncomment line below to see compiler error
-		//Shape shape = new Shape();
-		List<Shape> shapes = asList(new Circle(), new Square(), new Triangle());
+//		Shape shape = new Shape();
+		List<Shape> shapes = asList(new Circle(), new Square(), new Triangle(), new Rectangle());
 		for (Shape shape : shapes) {
 			shape.draw();
 		}
