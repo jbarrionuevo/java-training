6b6eb919a058e39b028f886776c3ecb26a454cd0
unknown <dante.salamone@GLB-D2120.globant.com>
2016-06-16 15:52:31 -0300

    adding collections' exercises

---
+++ java-training-core/src/main/java/edu/globant/day2/collections/exercises/Inventory.java
@@ -0,0 +1,16 @@
+package edu.globant.day2.collections.exercises;
+
+import java.util.HashMap;
+
+public class Inventory {
+	private HashMap<Case,Integer> cases;
+
+	public HashMap<Case, Integer> getCases() {
+		return cases;
+	}
+
+	public void setCases(HashMap<Case, Integer> cases) {
+		this.cases = cases;
+	}
+	
+}
