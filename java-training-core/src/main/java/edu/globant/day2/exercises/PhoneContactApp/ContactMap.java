c599b4ba0acb8fdae3723a63883c583e4df580a5
federico.calarco <federico.calarco@GLB-D2068.globant.com>
2016-06-16 18:52:54 -0300

    Upload Exercises

---
+++ java-training-core/src/main/java/edu/globant/day2/exercises/PhoneContactApp/ContactMap.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.globant.day2.exercises.PhoneContactApp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author federico.calarco
+ */
+public class ContactMap {
+    
+    public static void main(String[]args){
+        
+        Map<String, Object> contactsMap = new HashMap<>();
+        
+        // CONTINUE ......
+    }
+    
+}
---
+++ java-training-core/src/main/java/edu/globant/day2/exercises/PhoneContactApp/ContactSet.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.globant.day2.exercises.PhoneContactApp;
+
+/**
+ *
+ * @author federico.calarco
+ */
+public class ContactSet {
+    
+}
