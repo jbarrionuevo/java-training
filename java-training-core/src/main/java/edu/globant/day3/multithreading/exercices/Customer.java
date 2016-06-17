7c1c919afc3296dfe61c340a5692b661bd2a6c63
Ilancho <ilanrosenfeld7@gmail.com>
2016-06-17 17:11:11 -0300

    adding multithreading exercices

--- java-training-core/src/main/java/edu/globant/day3/multithreading/exercices/Customer.java
+++ java-training-core/src/main/java/edu/globant/day3/multithreading/exercices/Customer.java
@@ -25,7 +25,7 @@ public class Customer{
 	
 	public void askForBorrowal(){
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(4000);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -41,7 +41,7 @@ public class Customer{
 	
 	public void depositMoney(){
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(5000);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
