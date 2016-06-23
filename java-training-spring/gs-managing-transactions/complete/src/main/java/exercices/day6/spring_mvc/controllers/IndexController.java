1a49ad410ded86a0657fd8b37d55098f7e991df9
ilan-rosenfeld <ilan.rosenfeld@globant.com>
2016-06-23 16:34:41 -0300

    adding spring security

--- java-training-spring/gs-managing-transactions/complete/src/main/java/exercices/day6/spring_mvc/controllers/IndexController.java
+++ java-training-spring/gs-managing-transactions/complete/src/main/java/exercices/day6/spring_mvc/controllers/IndexController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
     @RequestMapping("/")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="Guest") String name, Model model) {
         model.addAttribute("name", name);
         return "index";
+    }
+    
+    @RequestMapping("/login")
+    public String handleLogin() {
+		return "login";
     }
     
 }
