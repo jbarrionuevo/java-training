01dfb35cb280576618c57b45b968eb5b8115f44b
pablo1989 <pablo.pellecchia@globant.com>
2016-06-23 00:24:48 -0300

    Added uml diagrams to the design project

--- java-training-core/src/main/java/edu/globant/day3/multithreading/exercices/BankExercise.java
+++ java-training-core/src/main/java/edu/globant/day3/multithreading/exercices/BankExercise.java
@@ -1,5 +1,60 @@
 package edu.globant.day3.multithreading.exercices;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//class BankerThreads extends Thread {
+//	@Override
+//	public void run() {
+//		for (int i = 1; i <= LIMIT; i++) {
+//			System.out.println(String.format("Hilo: %s. Data: %d", this.getName(), i));
+//		}
+//	}
+//}
+
+class Customer {
+	
+	private Long accountNumber;
+	private int customerAction;
+	
+	public Customer(Long accountNumber, int action) {
+		this.accountNumber = accountNumber;
+		this.customerAction = action;
+	}
+	
+	public Long getNumber() {
+		return this.accountNumber;
+	}
+	
+	public int getAction() {
+		return this.customerAction;
+	}
+}
+
+class Account {
+	private Long accountNumber;
+	private Double accountMoney;
+	
+	public Account(Long accountNumber) {
+		this.accountNumber = accountNumber;
+		this.accountMoney = 0.0;
+	}
+	
+	public void depositMoney(Double money) {
+		this.accountMoney = money;
+	}
+}
+
+class BankAccounts {
+	Map<Long, Double> customersAccounts;
+	
+	public BankAccounts() {
+		this.customersAccounts = new HashMap<Long, Double>();
+	}
+	
+}
+
 public class BankExercise {
 
+	List<>
 }
---
+++ java-training-design/src/main/java/edu/globant/design/exercices/uml/UmlDescriptioin.java
@@ -0,0 +1,18 @@
+package edu.globant.design.exercices.uml;
+
+public class UmlDescriptioin {
+
+	/*
+	 * I uploaded the uml files to the following links
+	 * 
+	 * Use Case Diagram: https://postimg.org/image/ciifmqlgn/
+	 * 
+	 * State Diagram: https://postimg.org/image/gsx3ibqjr/
+	 * 
+	 * Class Diagram: https://postimg.org/image/42sv58ilj/
+	 * 
+	 * Sequence Diagram: https://postimg.org/image/rv26grkmf/
+	 * 
+	 * 
+	 * */
+}
