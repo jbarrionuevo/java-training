ba1985c46e5bb8fd28dce1264c4e00e27ee58f48
ilan-rosenfeld <ilan.rosenfeld@globant.com>
2016-06-23 11:31:12 -0300

    adding info

--- java-training-spring/gs-managing-transactions/complete/src/main/java/exercices/day6/dependency_injection/services/SaleService.java
+++ java-training-spring/gs-managing-transactions/complete/src/main/java/exercices/day6/dependency_injection/services/SaleService.java
@@ -2,15 +2,20 @@ package exercices.day6.dependency_injection.services;
 
 import java.util.HashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import exercices.day6.dependency_injection.daos.InventoryDao;
 import exercices.day6.dependency_injection.daos.SaleDao;
+import exercices.day6.dependency_injection.models.Sale;
 
 @Service
 public class SaleService {
-	
+	private final static Logger log = LoggerFactory.getLogger(SaleService.class);
+
 	@Autowired
 	private SaleDao saleDao;
 	
@@ -20,6 +25,14 @@ public class SaleService {
 	
 	public SaleDao getSaleDao() {
 		return saleDao;
+	}
+	
+	@Transactional
+	public void registerSales(Sale... sales){
+		 for (Sale sale: sales) {
+	            log.info("Persisting Order [" + sale + "] to database...");
+	            saleDao.save(sale);
+	     }
 	}
 
 	public void setSaleDao(SaleDao saleDao) {
