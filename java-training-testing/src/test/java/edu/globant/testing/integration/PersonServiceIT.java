f954a7c51cb1597a52bfa63f246176615bb2ad7e
Carlos Curotto <carloscurotto@gmail.com>
2016-06-24 12:11:01 -0300

    Adds unit and integration testing examples

---
+++ java-training-testing/src/test/java/edu/globant/testing/integration/PersonServiceIT.java
@@ -0,0 +1,27 @@
+package edu.globant.testing.integration;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class PersonServiceIT {
+
+    private PersonService service;
+
+    @Before
+    public void setUp() {
+        service = new PersonService(new PersonDAO());
+    }
+
+    @Test
+    public void storeAndRetrieve() {
+        service.create("test");
+        Person person = service.findBy("test");
+
+        assertThat("The person just created and retrieved is null", person, notNullValue());
+        assertThat("Wrong person just created and retrieved", person.getName(), equalTo("test"));
+    }
+
+}
