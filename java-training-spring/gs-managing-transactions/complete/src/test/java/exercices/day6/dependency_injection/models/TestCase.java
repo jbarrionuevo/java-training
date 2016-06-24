17dc976f21d601f94a3f8924d461e60b518caff2
ilan-rosenfeld <ilan.rosenfeld@globant.com>
2016-06-24 13:52:00 -0300

    adding test package

---
+++ java-training-spring/gs-managing-transactions/complete/src/test/java/exercices/day6/dependency_injection/models/TestCase.java
@@ -0,0 +1,14 @@
+package exercices.day6.dependency_injection.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
