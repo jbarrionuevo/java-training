59eb2bc5426bf1d3a53b6cc4c58ca7829077935d
Ilancho <ilanrosenfeld7@gmail.com>
2016-06-29 21:58:30 -0300

    adding sale rest controller

--- java-training-final-project/src/main/java/ilan/restcontrollers/SaleRestController.java
+++ java-training-final-project/src/main/java/ilan/restcontrollers/SaleRestController.java
@@ -1,10 +1,33 @@
 package ilan.restcontrollers;
 
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import ilan.dtos.SaleDTO;
+import ilan.models.Sale;
+import ilan.services.SaleService;
 
 @RestController
 @RequestMapping("/sales")
 public class SaleRestController {
-
+	
+	@Autowired
+	SaleService saleService;
+	
+	@Autowired
+	private Mapper mapper;
+	
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(value= HttpStatus.CREATED)
+	public SaleDTO save(@RequestBody SaleDTO saleDTO){
+		Sale newSale=saleService.save(mapper.map(saleDTO,Sale.class));
+		SaleDTO savedEntityDTO = mapper.map(newSale, SaleDTO.class);
+		return savedEntityDTO;
+	}
 }
