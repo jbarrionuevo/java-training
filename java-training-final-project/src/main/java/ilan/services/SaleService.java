35e8a4d95a006aa12997c4a40dfe16a94c9327bd
Ilancho <ilanrosenfeld7@gmail.com>
2016-06-29 21:58:56 -0300

    changing saleService

--- java-training-final-project/src/main/java/ilan/services/SaleService.java
+++ java-training-final-project/src/main/java/ilan/services/SaleService.java
@@ -1,14 +1,11 @@
 package ilan.services;
 
-import java.util.HashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ilan.daos.InventoryDao;
 import ilan.daos.SaleDao;
 import ilan.models.Sale;
 
@@ -19,16 +16,16 @@ public class SaleService {
 	@Autowired
 	private SaleDao saleDao;
 	
-	@Autowired
-	private InventoryDao inventoryDao;
-
-	
 	@Transactional
 	public void registerSales(Sale... sales){
 		 for (Sale sale: sales) {
 	            log.info("Persisting Order [" + sale + "] to database...");
 	            saleDao.save(sale);
 	     }
+	}
+	
+	public Sale save(Sale newSale){
+	    return saleDao.save(newSale);
 	}
 
 	
