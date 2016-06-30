382435d0d598b1f868f63707ba5579f95bef225d
pocho <pocho83@gmail.com>
2016-06-29 21:32:47 -0300

    jpa exercises

---
+++ java-training-core/src/main/java/edu/globant/day4/jpa/exercises/InventoryDaoJPA.java
@@ -0,0 +1,34 @@
+package edu.globant.day4.jpa.exercises;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import edu.globant.day1.ddd.exercises.model.Inventory;
+
+public class InventoryDaoJPA {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public EntityManager getEntityManager() {
+		return em;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.em = entityManager;
+	}
+
+	public void insert(Inventory inventory) {
+		em.persist(inventory);
+	}
+
+	public List<Inventory> findAll() {
+		return em.createQuery("from Inventory").getResultList();
+	}
+
+	public Inventory update(Inventory inventory) {
+		return em.merge(inventory);
+	}
+}
