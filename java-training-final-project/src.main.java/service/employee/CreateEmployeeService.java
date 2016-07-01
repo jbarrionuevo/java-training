309f6824391c7696d074e96a70639985d7c083fc
jorge.barrionuevo <jorge.barrionuevo@globant.com>
2016-07-01 17:09:23 -0300

    Adding Spring transactions and JDBC to Final Project

--- java-training-final-project/src.main.java/service/employee/CreateEmployeeService.java
+++ java-training-final-project/src.main.java/service/employee/CreateEmployeeService.java
@@ -1,16 +1,19 @@
 package service.employee;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import domain.Employee;
 import persistence.DAO.EmployeeDAO;
 
 public class CreateEmployeeService {
 
 	private EmployeeDAO employeeDAO;
-
+	
 	public CreateEmployeeService(EmployeeDAO employeeDAO) {
 		this.employeeDAO = employeeDAO;
 	}
 
+	@Transactional	
 	public void create(Employee employee) {;
 		employeeDAO.save(employee);
 	}
