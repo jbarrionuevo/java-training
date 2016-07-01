fe931462a274e64cfeb7ab6e01f1d04faf538af3
ilan-rosenfeld <ilan.rosenfeld@globant.com>
2016-07-01 12:30:26 -0300

    paying/cancelling sales

--- java-training-final-project/src/main/webapp/js/sales.js
+++ java-training-final-project/src/main/webapp/js/sales.js
@@ -1,14 +1,40 @@
 $(document).ready(function(){  
 	
 	$(document).on("click",".confirmSale",function(){
+		var saleId = this.id;
 		 if (confirm('Sure to confirm the sale?')) {
-			 alert("sale confirmed");
+			 updateSale("paid");
 		 }
 	});
 	
 	$(document).on("click",".cancelSale",function(){
+		var saleId = this.id;
 		 if (confirm('Sure to cancel the sale?')) {
-			 alert("sale cancelled");
+			 updateSale("cancelled");
 		 }
 	});
 });
+
+function updateSale(status){
+	$.ajax({
+		   url: '/sales/'+saleId+"?status="+status,
+//		   headers: { 
+//		        'Accept': 'application/json',
+//		        'Content-Type': 'application/json' 
+//		   },
+		   data: {
+			 saleId = saleId
+		   },
+		   error: function() {
+		      $('#result').html('<p>An error has occurred</p>');
+		   },
+		   success: function(data) {
+			   updateView(data);
+		   },
+		   type: 'PUT'
+	});
+}
+
+function updateView(data){
+	
+}
